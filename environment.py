0156c334294205af214e7637467fff3f5d5b5d29
Adam Struharnansky <adam.struharnansky@gmail.com>
2023-01-19 22:20:14 +0100

    Create environment.py
    Simple enviroment to simulate pipes and filters

---
+++ environment.py
@@ -0,0 +1,25 @@
+
+
+class SimpleEnvironment:
+    def __init__(self, filters):
+        print('here')
+        self.filters = filters
+
+    def add_filter(self, new_filter):
+        self.filters.append(new_filter)
+
+    def clear_values(self):
+        pass
+
+    def run(self, interval, step):
+        self.clear_values()
+        current_step = 0
+        while current_step < interval:
+            for f in self.filters:
+                f.set_time(current_step)
+            for f in self.filters:
+                f.apply_function()
+            for f in self.filters:
+                f.sent_values()
+            current_step += step
+
